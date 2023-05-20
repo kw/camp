0ea1dd0e84603660d02eb4963769a0daa0cef44d
Brian Furlong <jalaketu@icloud.com>
2023-05-20 12:17:16 -0600

    Created models.py in accounts

---
+++ camp/accounts/models.py
@@ -0,0 +1,10 @@
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
+
+# create an Account model
+
+# create a Membership model, with refrences to
+    # Game (foreignKey)
+    # User (foreignKey)
+    # Nickname
+    # Date joined

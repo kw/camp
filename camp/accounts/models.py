c4347a89632aa6bafece502737747b83fe4a8c13
Brian Furlong <jalaketu@icloud.com>
2023-05-21 23:59:24 -0600

    Built Membership model in accounts.models

--- camp/accounts/models.py
+++ camp/accounts/models.py
@@ -1,10 +1,21 @@
-# from django.contrib.auth import get_user_model
-# User = get_user_model()
+from django.contrib.auth import get_user_model
+from django.db import models
+from game.models import Game
 
-# create an Account model
+User = get_user_model()
 
-# create a Membership model, with refrences to
-# Game (foreignKey)
-# User (foreignKey)
-# Nickname
-# Date joined
+
+class Membership(models.Model):
+    """Represents a user's relationship with a game.
+
+    A user can be a member of one or more games.
+    A user gives a particular game only what information they want,
+    so each game membership has its own profile data,
+    though later we might add a feature to copy data between a user's
+    profiles or otherwise keep them in sync.
+    """
+
+    joined: int = models.TimeField(auto_now_add=True)
+    nickname: str = models.CharField(blank=True, max_length=50, default="nickname")
+    game: Game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="game")
+    user: User = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
